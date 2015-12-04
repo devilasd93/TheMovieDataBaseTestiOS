9e8885cd30fde69cad7e0c96657223b029ed0235
Dima <Buza.dima93@gmail.com>
2015-12-04 10:08:58 +0600

    Initial Commit

---
+++ TheMovieDataBaseTestObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheMovieDataBaseTestObj
+//
+//  Created by Dmitriy on 04.12.15.
+//  Copyright © 2015 Dmitriy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheMovieDataBaseTestObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheMovieDataBaseTestObj
+//
+//  Created by Dmitriy on 04.12.15.
+//  Copyright © 2015 Dmitriy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
